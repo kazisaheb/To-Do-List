c5756a9a61f5faa972db9297a1c797c2491f8a4c
unknown <you@example.com>
2022-02-16 20:03:21 +0600

    Remove button active

--- todo.js
+++ todo.js
@@ -2,19 +2,12 @@ let toDos = document.getElementById('toDos');
 let list = document.getElementById('list');
 let demoText = document.getElementById('demoText');
 let inputText = document.getElementById('inputText');
+let single = document.getElementById('single');
 
 // Add list
 function input() {
   if (inputText.value != '') {
     let listDiv = document.createElement('div');
-
-    // listDiv.setAttribute('class', 'single')
-    // listDiv.innerHTML = `
-    // <p>Hello</p>
-    //  <button>Add</button>
-    // `
-    // list.appendChild(listDiv)
-
     listDiv.innerHTML = list.innerHTML;
     toDos.appendChild(listDiv);
     demoText.innerText = inputText.value;
@@ -24,23 +17,20 @@ function input() {
 }
 
 // Edit button
-let edit = document.getElementById('edit');
-edit.addEventListener('click', function () {
-  console.log('Edit clicked');
-  demoText.innerText = 'Edited successfully!!'
-})
+function edit(event) {
+  event.target.parentNode.parentNode.parentNode.demoText(event.target.parentNode.parentNode).innerText = 'Edited successfully!!'
+
+  // demoText.innerText = 'Edited successfully!!'
+}
 
 // List bg change
-let single = document.getElementById('single');
-let color = document.getElementById('color');
-color.addEventListener('click', function () {
+function color() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   single.style.backgroundColor = "#" + randomColor;
   single.style.color = 'white';
-})
+}
 
 // Remove item
-let cross = document.getElementById('cross');
-cross.addEventListener('click', function (event) {
+function Close(event) {
   event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
-})
+}
