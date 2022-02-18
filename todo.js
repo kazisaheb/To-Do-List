498c4fe77e6f4899da69bca0547d7ca2993430e1
unknown <you@example.com>
2022-02-18 23:07:18 +0600

    Tried & faild with localstorage

--- todo.js
+++ todo.js
@@ -4,16 +4,19 @@ let demoText = document.getElementById('demoText');
 let inputText = document.getElementById('inputText');
 let single = document.getElementById('single');
 
+// localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
+// let getField = JSON.parse(localStorage.getItem('field'));
+// console.log(getField);
+
 // Add list
 function input() {
   if (inputText.value != '') {
     let listDiv = document.createElement('div');
     listDiv.innerHTML = list.innerHTML;
-    toDos.appendChild(listDiv);
+    toDos.append(listDiv);
     demoText.innerText = inputText.value;
     inputText.value = '';
     single.style.display = 'block';
-    localStorage.setItem('div', toDos);// Testing
   }
 }
 
@@ -28,5 +31,4 @@ function color(event) {
 // Remove item
 function Close(event) {
   event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
-  localStorage.removeItem('listDiv'); //Testing
 }
