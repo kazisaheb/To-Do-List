4fc03715231e11c3f9b8eb2a63decb2e5fe9c385
unknown <you@example.com>
2022-02-25 22:41:42 +0600

    Alomost done except color

--- todo.js
+++ todo.js
@@ -3,23 +3,26 @@ let list = document.getElementById('list');
 let demoText = document.getElementById('demoText');
 let inputText = document.getElementById('inputText');
 let single = document.getElementById('single');
-
-// localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
-// let getField = JSON.parse(localStorage.getItem('field'));
-// console.log(getField);
-
-// Add list
-function input() {
+// Store
+input = () => {
   if (inputText.value != '') {
     let listDiv = document.createElement('div');
-    listDiv.innerHTML = list.innerHTML;
+    listDiv.classList.add('list')
+    listDiv.innerHTML = `   
+    <div id="single">
+      <div class="options">
+        <span onclick="Close(event)" id="cross">Close</span>
+        <span onclick="color(event)" id="color">Color</span>
+      </div>
+      <p id="demoText">${inputText.value}</p>
+    </div>
+    `
     toDos.append(listDiv);
-    demoText.innerText = inputText.value;
     inputText.value = '';
-    single.style.display = 'block';
+    localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
   }
 }
-
+toDos.innerHTML = JSON.parse(localStorage.getItem('field'));
 // color button
 function color(event) {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -27,8 +30,13 @@ function color(event) {
   bg.style.backgroundColor = "#" + randomColor;
   bg.style.color = 'white';
 }
-
 // Remove item
 function Close(event) {
-  event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
+  event.target.parentNode.parentNode.parentNode.remove();
+  localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
 }
+// Clear All
+clearAll = () => {
+  localStorage.clear()
+  toDos.innerHTML = ''
+} 
