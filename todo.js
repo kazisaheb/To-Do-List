28a800aa7defd3d033b8b93ffd058e3004d6ad3c
unknown <you@example.com>
2022-02-28 10:43:46 +0600

    Code formated

--- todo.js
+++ todo.js
@@ -1,17 +1,17 @@
-let toDos = document.getElementById('toDos');
-let list = document.getElementById('list');
-let demoText = document.getElementById('demoText');
-let inputText = document.getElementById('inputText');
-let single = document.getElementById('single');
-// Store
+let toDos = document.getElementById('toDos')
+let list = document.getElementById('list')
+let demoText = document.getElementById('demoText')
+let inputText = document.getElementById('inputText')
+let single = document.getElementById('single')
+// Append
 input = () => {
   if (inputText.value != '') {
-    let listDiv = document.createElement('div');
+    let listDiv = document.createElement('div')
     listDiv.classList.add('list')
     listDiv.innerHTML = `   
     <div id="single">
       <div class="options">
-        <span onclick="xxxx(event)" id="cross">Remove</span>
+        <span onclick="remove(event)" id="cross">Remove</span>
         <span onclick="edit(event)" id="editBtn" class="editBtn">Edit</span>
         <span onclick="save()" id="saveBtn">Save</span>
         <span onclick="color(event)" id="color">Color</span>
@@ -20,24 +20,26 @@ input = () => {
       <span id="dates">${new Date().toDateString()}, ${new Date().toLocaleTimeString()}</span>    
     </div>
     `
-    toDos.append(listDiv);
-    inputText.value = '';
-    localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
+    toDos.append(listDiv)
+    inputText.value = ''
+    store()
   }
 }
-toDos.innerHTML = JSON.parse(localStorage.getItem('field'));
-// color button
+// LocalStorage function
+store = () => localStorage.setItem('field', JSON.stringify(toDos.innerHTML))
+toDos.innerHTML = JSON.parse(localStorage.getItem('field'))
+// Color button
 function color(event) {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  let bg = event.target.parentNode.parentNode;
-  bg.style.backgroundColor = "#" + randomColor;
-  bg.style.color = 'white';
-  localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+  let bg = event.target.parentNode.parentNode
+  bg.style.backgroundColor = "#" + randomColor
+  bg.style.color = 'white'
+  store()
 }
 // Remove item
-function xxxx(event) {
-  event.target.parentNode.parentNode.parentNode.remove();
-  localStorage.setItem('field', JSON.stringify(toDos.innerHTML));
+function remove(event) {
+  event.target.parentNode.parentNode.parentNode.remove()
+  store()
 }
 // Clear All
 clearAll = () => {
@@ -46,7 +48,7 @@ clearAll = () => {
 }
 // Edit Button finction
 edit = (event) => {
-  let theNote = event.target.parentNode.parentNode.childNodes[3];
+  let theNote = event.target.parentNode.parentNode.childNodes[3]
   let newText = document.createElement('textarea')
   newText.classList.add('newText')
   newText.value = theNote.innerText
@@ -62,6 +64,6 @@ edit = (event) => {
     newText.style.display = 'none'
     saveBtn.style.display = 'none'
     event.target.style.display = 'block'
-    localStorage.setItem('field', JSON.stringify(toDos.innerHTML))
+    store()
   }
 }
